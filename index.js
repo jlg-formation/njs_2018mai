5e1ab672c1e11ba47cd563b53565d2af9d7d2987
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-05-15 16:16:20 +0200

    ok

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ const fs = require('fs');
 
 console.log('start');
 
-setTimeout(()=>{
-    const buffer = fs.readFileSync('./package.json');
+const dormir = (temps) => {
+    return new Promise((succesAppel, erreurAppel) => {
+        if (temps > 4000) {
+            erreurAppel(`tu as depasse 4000. (${temps})`);
+        } else {
+            setTimeout(succesAppel, temps);
+        }
+    });
+};
 
-    console.log(buffer.toString());
-}, 2000);
 
+const main = () => {
+    dormir(8000).then(() => {}).catch((err) => {
+        console.log('erreur', err);
+    });
+};
+
+
+main();
 
